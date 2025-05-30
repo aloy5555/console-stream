f9fbb303a1b338ad22cc0aa8966091c4f247c126
aloy555 <hugo@ritger.nl>
2025-05-30 07:28:42 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,28 +1,88 @@
 @echo off
-:: Install Arduino IDE
-echo Installing Arduino IDE...
-start /wait https://downloads.arduino.cc/arduino-1.8.19-windows.exe
+REM === GP2040-CE Windows Automation Script for Switch/PS4/PS5 Remote Play with Moonlight & Pico ===
+REM - Streams console video using Sunshine & HDMI capture card
+REM - Forwards controller input from Moonlight client to Pico (GP2040-CE) emulating your chosen console
 
-:: Install ESP32 Board Support
-echo Installing ESP32 Board Support...
-start /wait https://dl.espressif.com/dl/package_esp32_index.json
+REM -------- USER CONFIGURATION --------
+REM Set your Pico's IP address here:
+set PICO_IP=192.168.1.100
 
-:: Install Bluepad32 Library
-echo Installing Bluepad32 Library...
-start /wait https://github.com/ricardoquesada/bluepad32/releases/download/v3.7.0/bluepad32-3.7.0.zip
+REM Set your HDMI capture device. Example: video=video=USB Video or the device index (check in ffplay/OBS)
+set HDMI_DEVICE=video="YOUR HDMI DEVICE NAME HERE"
 
-:: Install Sunshine
-echo Installing Sunshine...
-start /wait https://github.com/LizardByte/Sunshine/releases/download/v0.15.0/Sunshine-0.15.0-Setup.exe
+REM -------- SYSTEM CHECKS --------
+echo.
+echo ============================================
+echo   GP2040-CE + Sunshine Windows Automation
+echo ============================================
+echo.
+echo 1. Make sure you have:
+echo    - [x] Installed ViGEmBus (https://vigembus.github.io/)
+echo    - [x] Sunshine installed (https://sunshineapp.dev/)
+echo    - [x] Python 3.x (https://www.python.org/)
+echo    - [x] pip installed for Python
+echo    - [x] Moonlight client with Virtual Gamepad enabled
+echo 2. Connect your Pico (with GP2040-CE) to your console's USB port.
+echo 3. Use the same Pico IP in this script and in the Python script.
+echo.
 
-:: Configure Sunshine
-echo Configuring Sunshine...
-start /wait "" "C:\Program Files\Sunshine\Sunshine.exe" --configure
+REM -------- CHOOSE CONSOLE TO EMULATE --------
+:choose_console
+echo Which console do you want your Pico/GP2040-CE to emulate?
+echo [1] Nintendo Switch (Pro Controller)
+echo [2] PlayStation 4 (DualShock 4)
+echo [3] PlayStation 5 (DualSense)
+set /p CONSOLE_CHOICE="Enter 1, 2, or 3: "
+if "%CONSOLE_CHOICE%"=="1" (
+    set CONSOLE_MODE=switch
+) else if "%CONSOLE_CHOICE%"=="2" (
+    set CONSOLE_MODE=ps4
+) else if "%CONSOLE_CHOICE%"=="3" (
+    set CONSOLE_MODE=ps5
+) else (
+    echo Invalid choice. Please enter 1, 2, or 3.
+    goto choose_console
+)
 
-:: Upload ESP32 Firmware
-echo Uploading ESP32 Firmware...
-start /wait "" "C:\Program Files\Arduino\arduino.exe" --upload "C:\path\to\your\firmware\Bluepad32.ino"
+echo.
+echo You selected: %CONSOLE_MODE%
+echo.
+pause
 
-:: Done
-echo Setup Complete!
+REM -------- PYTHON DEPENDENCIES --------
+echo Installing Python dependencies...
+pip install inputs requests
+
+REM -------- DOWNLOAD CONTROLLER SENDER SCRIPT --------
+set SCRIPT_DIR=%~dp0
+set DEMO_SCRIPT=%SCRIPT_DIR%controller_to_gp2040ce.py
+
+REM Download always-latest version; replace with your own repo if you fork/customize
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/aloy5555/tools-public/main/controller_to_gp2040ce.py -OutFile '%DEMO_SCRIPT%'"
+
+REM -------- PATCH PICO IP AND CONSOLE MODE IN SCRIPT --------
+powershell -Command "(Get-Content '%DEMO_SCRIPT%') -replace \"PICO_IP = '.*'\", \"PICO_IP = '%PICO_IP%'\" | Set-Content '%DEMO_SCRIPT%'"
+powershell -Command "(Get-Content '%DEMO_SCRIPT%') -replace \"CONSOLE_MODE = '.*'\", \"CONSOLE_MODE = '%CONSOLE_MODE%'\" | Set-Content '%DEMO_SCRIPT%'"
+
+REM -------- START SUNSHINE --------
+echo Starting Sunshine...
+start "" "C:\Program Files\Sunshine\sunshine.exe"
+
+REM -------- LAUNCH CONTROLLER SENDER --------
+echo Starting controller-to-GP2040-CE bridge (emulating %CONSOLE_MODE%)...
+start cmd /k python "%DEMO_SCRIPT%"
+
+REM -------- INSTRUCTIONS FOR HDMI VIEWER --------
+echo.
+echo =========================================================
+echo When setting up Sunshine:
+echo   - In the Sunshine web UI, add a new "Application" entry.
+echo   - Command: ffplay -fflags nobuffer -flags low_delay -framedrop -i %HDMI_DEVICE% -window_title "ConsoleStream"
+echo     (You may need to install ffmpeg/ffplay for Windows: https://www.gyan.dev/ffmpeg/builds/)
+echo   - Save, then launch this app from Moonlight to view your console stream.
+echo.
+echo Your controller input from Moonlight will be sent to the console via the Pico emulating a %CONSOLE_MODE% controller.
+echo.
+echo When finished, close the ffplay window and stop the Python script.
+echo =========================================================
 pause
